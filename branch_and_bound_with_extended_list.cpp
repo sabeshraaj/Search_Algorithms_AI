a82b78c304defd51371b54f7b7b25f86259dc2cb
sabeshraaj <143479667+sabeshraaj@users.noreply.github.com>
2024-11-07 17:12:28 +0530

    Update branch_and_bound_with_extended_list.cpp

--- branch_and_bound_with_extended_list.cpp
+++ branch_and_bound_with_extended_list.cpp
@@ -3,38 +3,38 @@ using namespace std;
 vector<int>h;
 int n;
 int oracle;
-int beamwidth;
 vector<vector<int>>adj[7];
 void printpath(vector<int>path)
 {
-    cout<<"path : "<<" ";
+    cout<<"path : "<<endl;
     for(auto it:path)
         {
             cout<<it<<" ";
         }
-    cout<<endl;
     cout<<endl;
 }
 void bbext()
 {
     vector<bool>vis(n,false);
     vector<int>dist(n,1e9);
-    int cur=0;//start
+    int cur=0;
     int goal=6;
     set<tuple<int,int,vector<int>>>st;
     st.insert({0,0,{0}});
     while(!st.empty())
     {
-       auto[dis,elem,path]=*st.begin();
-       st.erase(st.begin());
+        auto t=*st.begin();
+        int dis = get<0>(t);
+        int elem = get<1>(t); 
+        vector<int> path = get<2>(t);
+        st.erase(st.begin());
         if(vis[elem]){continue;}
         vis[elem]=true;
         dist[elem]=dis;
-        cout<<"cur elem: "<<elem<<"  dist : "<<dis<<endl;
+        cout<<"current element : "<<elem<<" distance covered : "<<dis<<endl;
         printpath(path);
         if(elem==goal)
         {
-            cout<<"goal reached, the final path is:"<<endl;
             printpath(path);
             return;
         }
@@ -56,7 +56,6 @@ void ae(int u,int v,int wt=1)
 }
 int main()
 {
-    oracle=100;
     h={4,3,3,1,1,1,0};
     ae(0,1);
     ae(0,2);
@@ -67,8 +66,7 @@ int main()
     ae(4,6);
     ae(5,6);
     n=7;
-    beamwidth=2;
-    cout<<"Branch and bound with extended list ALGORITHM : "<<endl;
-    cout<<endl;
+    oracle=100;//put your own value
+    cout<<"Branch and bound with extended list algorithm : "<<endl;
     bbext();
 }
