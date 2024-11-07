8dbae8386cf42681a26b1a75d8c046ea4d8fefca
sabeshraaj <143479667+sabeshraaj@users.noreply.github.com>
2024-11-07 16:35:20 +0530

    beam_search.cpp

--- beam_search.cpp
+++ beam_search.cpp
@@ -4,58 +4,67 @@ vector<int>h;
 int n;
 int beamwidth;
 vector<vector<int>>adj[7];
-void printpath(vector<int>path)
-{
-    cout<<"path: "<<endl;
-    for(auto i:path)
-    {
-        cout<<i<<" ";
-    }
-    cout<<endl;
-}
 void beam()
 {
     vector<bool>vis(n,false);
     vector<int>dist(n,1e9);
     int cur=0;
     int goal=6;
-    set<tuple<int,int,vector<int>>>st;
-    st.insert({0,0,{0}});
+    set<pair<int,vector<int>>>st;
+    st.insert({0,{0}});
     while(!st.empty())
     {
-        auto[dis,elem,path]=*st.begin();
+        int dis=st.begin()->first;
+        vector<int>path=st.begin()->second;
+        int elem=path.back();
         st.erase(st.begin());
-        if(vis[elem])
-        {
-            continue;
-        }
+        if(vis[elem]){continue;}
         vis[elem]=true;
+        cout<<"at node: "<<elem<<endl;
+        cout<<"path till now : "<<endl;
+        for(auto it:path)
+        {
+            cout<<it<<" ";
+        }
+        cout<<endl;
         dist[elem]=dis;
         if(elem==goal)
         {
-            printpath(path);
+            cout<<"goal reached, final path is: "<<endl;
+            for(auto it:path)
+            {
+                cout<<it<<" ";
+            }
+            cout<<endl;
             return;
         }
-        for(auto i:adj[elem])
+        vector<pair<int,vector<int>>>temp;
+        for(auto it:adj[elem])
         {
-            int edw=i[1];
-            int node=i[0];
+            int edw=it[1];
+            int node=it[0];
             vector<int>np=path;
             np.push_back(node);
-            st.insert({edw+dis,node,np});
+            if(!vis[node])
+            {
+                temp.push_back({edw,np});
+            }
+        }
+        sort(temp.begin(),temp.end());
+        for(int i=0;i<min(beamwidth, (int)temp.size());i++)
+        {
+            st.insert(temp[i]);
         }
     }
     cout<<"path not found"<<endl;
 }
-void ae(int x,int y, int wt=1)
-{
-    adj[x].push_back({y,wt});
-    adj[y].push_back({x,wt});
+void ae(int u,int v,int wt=1)
+{    adj[u].push_back({v,wt});
+    adj[v].push_back({u,wt});
 }
 int main()
 {
     h={4,3,3,1,1,1,0};
-    n=7;
     ae(0,1);
     ae(0,2);
     ae(1,3);
@@ -64,6 +73,8 @@ int main()
     ae(3,6);
     ae(4,6);
     ae(5,6);
+    n=7;
     beamwidth=2;
+    cout<<"beam search algorithm: "<<endl;
     beam();
 }
