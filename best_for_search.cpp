cb895459f22eeb2a11c6dbe37c870a1b93871357
sabeshraaj <143479667+sabeshraaj@users.noreply.github.com>
2024-11-07 17:34:49 +0530

    best_for_search.cpp

--- best_for_search.cpp
+++ best_for_search.cpp
@@ -7,12 +7,11 @@ int beamwidth;
 vector<vector<int>>adj[7];
 void printpath(vector<int>path)
 {
-    cout<<"path : "<<" ";
+    cout<<"path : "<<endl;
     for(auto it:path)
         {
             cout<<it<<" ";
         }
-    cout<<endl;
     cout<<endl;
 }
 void best()
@@ -21,20 +20,22 @@ void best()
     vector<int>dist(n,1e9);
     int cur=0;
     int goal=6;
-    set<tuple<int,int,vector<int>>>st;
-    st.insert({0,0,{0}});
+    set<pair<int,pair<int,vector<int>>>>st; //dis, elem, path
+    st.insert({h[0],{0,{0}}});
     while(!st.empty())
     {
-       auto[dis,elem,path]=*st.begin();
-       st.erase(st.begin());
+        auto[dis,path1]=*st.begin();
+        int elem=path1.first;
+        vector<int>path=path1.second;
+        st.erase(st.begin());
         if(vis[elem]){continue;}
         vis[elem]=true;
         dist[elem]=dis;
-        cout<<"node: "<<elem<<" dis: "<<dis<<endl;
+        cout<<"node: "<<elem<<" distance covered: "<<dis<<endl;
         printpath(path);
         if(elem==goal)
         {
-            cout<<"goal reached, the final path is"<<endl;
+            cout<<"goal reached, final path:"<<endl;
             printpath(path);
             return;
         }
@@ -43,18 +44,18 @@ void best()
             int edw=it[1]; int node=it[0];
             vector<int>np=path;
             np.push_back(node);
-            st.insert({dis+edw,node,np});
+            st.insert({h[node],{node,np}});
         }
     }
     cout<<"path not found"<<endl;
 }
 void ae(int u,int v,int wt=1)
-{    adj[u].push_back({v,wt});
+{    
+    adj[u].push_back({v,wt});
     adj[v].push_back({u,wt});
 }
 int main()
 {
-    oracle=100;
     h={4,3,3,1,1,1,0};
     ae(0,1);
     ae(0,2);
@@ -65,7 +66,6 @@ int main()
     ae(4,6);
     ae(5,6);
     n=7;
-    beamwidth=2;
     cout<<"Best first search algorithm : "<<endl;
     best();
 }
